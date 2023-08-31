2d48011a2f0e26531babcafdfbf689da7328b46e
manas <bagrimanasbir@gmail.com>
2023-08-31 01:31:13 -0700

    a

---
+++ contracts/src/Donatooor.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Donatooor {
+    enum Platform {
+        Endaoment,
+        Gitcoin,
+        Drips
+    }
+
+
+
+
+    function donate(Platform platform) public payable {
+        // do nothing
+    }
+}
